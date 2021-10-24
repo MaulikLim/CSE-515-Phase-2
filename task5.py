484d41f82a53f5e18f4fb1dac16ec1358fe2e7ac
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2021-10-23 22:17:20 -0700

    feature-loader

--- task5.py
+++ task5.py
@@ -9,6 +9,7 @@ import json
 import os
 import numpy as np
 import datetime
+import featureLoader
 from tech.SVD import SVD
 from tech.LDA import LDA
 import tech.LDAHelper as lda_helper
@@ -43,27 +44,32 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-data = imageLoader.load_images_from_folder(args.folder_path)
+file_name = args.latent_path.split("/")[-1]
+info = file_name.split("_")
+model = modelFactory.get_model(info[2])
+tech = info[3].lower()
+type = info[4]
+l_k = info[5]
+data = featureLoader.load_features_for_model(args.folder_path, info[2])
 if data is not None:
-    file_name = args.latent_path.split("/")[-1]
-    info = file_name.split("_")
-    model = modelFactory.get_model(info[2])
-    tech = info[3].lower()
-    type = info[4]
-    l_k = info[5]
     if tech == 'pca':
         l_features = load_json(args.latent_path)
         labels = data[0]
         l_mat = np.array(l_features[1][0])
+        # if type == 'type' or type == 'subject':
+        #     feature_type_mat = np.array(l_features[3])
+        # new_data = []
+        # for d in data[1]:
+        #     feature_mat = model.compute_features(d)
+        #     if type == 'type' or type == 'subject':
+        #         feature_mat = np.matmul(feature_mat, feature_type_mat.T)
+        #     l_feature_mat = np.matmul(feature_mat, l_mat)
+        #     new_data.append(l_feature_mat)
+        feature_mat = data[1]
         if type == 'type' or type == 'subject':
             feature_type_mat = np.array(l_features[3])
-        new_data = []
-        for d in data[1]:
-            feature_mat = model.compute_features(d)
-            if type == 'type' or type == 'subject':
-                feature_mat = np.matmul(feature_mat, feature_type_mat.T)
-            l_feature_mat = np.matmul(feature_mat, l_mat)
-            new_data.append(l_feature_mat)
+            feature_mat = np.matmul(feature_mat, feature_type_mat.T)
+        new_data = np.matmul(feature_mat, l_mat)
         q_feature_mat = model.compute_features(imageLoader.load_image(args.image_path))
         if type == 'type' or type == 'subject':
             q_feature_mat = np.matmul(q_feature_mat, feature_type_mat.T)
@@ -93,15 +99,20 @@ if data is not None:
         l_features = load_json(args.latent_path)
         labels = data[0]
         r_mat = np.array(l_features[1][2]).transpose()
+        # if type == 'type' or type == 'subject':
+        #     feature_type_mat = np.array(l_features[3])
+        # new_data = []
+        # for d in data[1]:
+        #     feature_mat = model.compute_features(d)
+        #     if type == 'type' or type == 'subject':
+        #         feature_mat = np.matmul(feature_mat, feature_type_mat.T)
+        #     l_feature_mat = np.matmul(feature_mat, r_mat)
+        #     new_data.append(l_feature_mat)
+        feature_mat = data[1]
         if type == 'type' or type == 'subject':
             feature_type_mat = np.array(l_features[3])
-        new_data = []
-        for d in data[1]:
-            feature_mat = model.compute_features(d)
-            if type == 'type' or type == 'subject':
-                feature_mat = np.matmul(feature_mat, feature_type_mat.T)
-            l_feature_mat = np.matmul(feature_mat, r_mat)
-            new_data.append(l_feature_mat)
+            feature_mat = np.matmul(feature_mat, feature_type_mat.T)
+        new_data = np.matmul(feature_mat, r_mat)
         q_feature_mat = model.compute_features(imageLoader.load_image(args.image_path))
         if type == 'type' or type == 'subject':
             q_feature_mat = np.matmul(q_feature_mat, feature_type_mat.T)
@@ -133,17 +144,14 @@ if data is not None:
         print(len(l_features))
         lda = LDA(file_name=file_name)
         labels = data[0]
-        original_metrics = model.compute_features_for_images(data[1])
+        # original_metrics = model.compute_features_for_images(data[1])
+        original_metrics = data[1]
         if type == 'type' or type == 'subject':
             transform_matrix = np.array(l_features[2]).T
             original_metrics = np.matmul(original_metrics, transform_matrix)
-            print(original_metrics.shape, end='')
-            print('intermediate transformation')
         else:
             original_metrics = lda_helper.transform_cm_for_lda(original_metrics)
         original_metrics = lda.transform_data(original_metrics)
-        print(original_metrics.shape, end='')
-        print('final transformation')
 
         q_feature_mat = model.compute_features(imageLoader.load_image(args.image_path))
         q_feature_mat = q_feature_mat.reshape([1, q_feature_mat.shape[0]])
@@ -151,12 +159,8 @@ if data is not None:
             q_feature_mat = np.matmul(q_feature_mat, transform_matrix)
         else:
             q_feature_mat = lda_helper.transform_cm_for_lda(q_feature_mat)
-        print(q_feature_mat.shape, end='')
-        print('query intermediate transformation')
 
         q_feature_mat = lda.transform_data(q_feature_mat)
-        print(q_feature_mat.shape, end='')
-        print('query final transformation')
         result = []
         for ind, d in enumerate(original_metrics):
             sim_score = np.sum(np.abs(d - q_feature_mat))
@@ -176,8 +180,8 @@ if data is not None:
         if type == 'type' or type == 'subject':
             feature_type_mat = np.array(l_features[3])
         data_cluster_index, data_in_latent_space, data_cluster_dist = [], [], []
-        for d in data[1]:
-            feature_mat = model.compute_features(d)
+        for feature_mat in data[1]:
+            # feature_mat = model.compute_features(d)
             if type == 'type' or type == 'subject':
                 feature_mat = np.matmul(feature_mat, feature_type_mat.T)
             min_dist_arr = np.sum((centroids - feature_mat) ** 2, axis=1)
@@ -190,6 +194,8 @@ if data is not None:
         # data_in_latent_space[np.arange(data_cluster_index.size), data_cluster_index] = data_cluster_dist
 
         query_features = model.compute_features(imageLoader.load_image(args.image_path))
+        if type == 'type' or type == 'subject':
+            query_features = np.matmul(query_features, feature_type_mat.T)
         min_dist_arr = np.sum((centroids - query_features) ** 2, axis=1)
         # min_dist_ctr = np.argmin(min_dist_arr)
         # query_in_latent_space = np.zeros(centroids.shape[0])
